3c796f484959ea634b9df6d00ef79accddd90ff2
Safi <139801311+safi-siddiqui-github@users.noreply.github.com>
2024-10-03 11:36:10 +0500

    Tailwind Setup Complete

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,16 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
+
+      <View className='flex-1'>
+        <Text>Not Found Page</Text>
+        <Link href={'/'}>GO HOME</Link>
+      </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Home() {
+    return (
+        <SafeAreaView className="flex-1 items-center justify-center">
+            <Text>Helllo Worlds</Text>
+        </SafeAreaView>
+    )
+}
